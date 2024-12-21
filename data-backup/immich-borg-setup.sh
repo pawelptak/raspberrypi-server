75fb7d80c1c8b1585e7e4c6375f5749f6b9de93e
pawelptak <97pawelptak@gmail.com>
2024-12-21 10:09:18 +0100

    path fix

--- data-backup/immich-borg-setup.sh
+++ data-backup/immich-borg-setup.sh
@@ -7,7 +7,7 @@ BACKUP_PATH="/mnt/ssd_backup/immich_backup"
 ### Local
 
 # Backup Immich database
-docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres > "$BACKUP_PATH"/database-backup/immich-database.sql
+docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres > "$BACKUP_PATH"/immich-database.sql
 # For deduplicating backup programs such as Borg or Restic, compressing the content can increase backup size by making it harder to deduplicate. If you are using a different program or still prefer to compress, you can use the following command instead:
 # docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgres | /usr/bin/gzip --rsyncable > "$UPLOAD_LOCATION"/database-backup/immich-database.sql.gz
 
